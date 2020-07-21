266e47372be55afc56e1ccc188f4bbe2739a790b
joko751214 <joko751214@gmail.com>
2020-07-21 08:11:04 +0800

    week5 edited addToCart function

--- week5/all.js
+++ week5/all.js
@@ -8,7 +8,7 @@ new Vue({
 			imageUrl: [],
 			num: 0,
 		},
-		cart: []
+    carts: {},
 	},
 	methods: {
 		getProducts(page = 1) {
@@ -22,7 +22,6 @@ new Vue({
 			},
 		openModal(item) {
       this.tempProduct = Object.assign({}, item)
-      console.log(this.tempProduct);
 			this.getProduct(this.tempProduct.id);
 		},
 		getProduct(id) {
@@ -34,46 +33,36 @@ new Vue({
           $("#tempProductModal").modal("show");
         })
     },
-    confirm(item, num=1) {
-      item.num = num;
-      this.cart.push(item);
+    addToCart(item, num=1) {
+      const url = `${this.apiPath}/${this.uuid}/ec/shopping`;
+
+      const parm = {
+        product: item.id,
+        quantity: num
+      };
+
+      axios.post(url, parm)
+        .then( res => {
+          console.log('post:', res)
+        })
+        .catch(err => {
+          console.log(err);
+        })
       $("#tempProductModal").modal("hide");
-      console.log(this.cart)
+    },
+    getCartData() {
+      const url = `${this.apiPath}/${this.uuid}/ec/shopping`;
+
+      axios.get(url)
+        .then( res => {
+          console.log('get:', res)
+        })
+        .catch(err => {
+          console.log(err);
+        })
     }
   },
 	created() {
 		this.getProducts();
 	},
 })
-
-
-// var obj = {
-// 	data: {
-// 		uuid: 'e9fd54ce-9d42-40cb-ba4e-14553d1d6375',
-// 		apiPath: 'https://course-ec-api.hexschool.io/api',
-// 		products: []
-// 	},
-// 	getData() {
-// 		var vm = this;
-// 		var url = `${vm.data.apiPath}/${vm.data.uuid}/ec/products`;
-// 		axios.get(url)
-// 			.then(function(res){
-// 				vm.data.products = res.data.data
-// 				vm.render()
-// 		})
-// 	},
-// 	render() {
-// 		var str = '';
-// 		var productList = document.querySelector(".product")
-// 		this.data.products.forEach(item => {
-// 			str += `<div class="col-sm col-md-4 col-sm-4"><img class = "img-fluid" src="${item.imageUrl}"/>
-// 				<div class="title">${item.title}</div>
-// 				<div class="price">${item.price}</div>
-// 				<button class="btn btn-outline-primary mt-2 float-left">查看內容</button>
-// 			<button class="btn btn-outline-danger mt-2 float-right">加入購物車</button>
-//     </div>`;
-// 		})
-// 		productList.innerHTML = str;
-// 	}
-// };
-// obj.getData();
